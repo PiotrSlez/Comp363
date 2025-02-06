fc5d79001300f875f0e249dcd305369f5c8d17a9
Piotr Slezak <151113027+PiotrSlez@users.noreply.github.com>
2025-02-06 13:25:14 -0600

    Update Main.java

--- Assignment 2/Main.java
+++ Assignment 2/Main.java
@@ -6,7 +6,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>(); //list with eight elements
+        LinkedList<Integer> list2 = new LinkedList<>(); //list with one element
+        LinkedList<Integer> list3 = new LinkedList<>(); //list without elements
+        
         list.add(1);
         list.add(23);
         list.add(1);
@@ -15,17 +18,18 @@ public class Main {
         list.add(2);
         list.add(67);
         list.add(8);
+
+        list2.add(8);
+
+        
         System.out.println("list before: " + list);
         list = MergeSort.mergeSort(list);
         System.out.println("list sorted: " + list);
 
-        LinkedList<Integer> list2 = new LinkedList<>();
-        list2.add(8);
         System.out.println("list before: " + list2);
         list2 = MergeSort.mergeSort(list2);
         System.out.println("list sorted: " + list2);
 
-        LinkedList<Integer> list3 = new LinkedList<>();
         System.out.println("list before: " + list3);
         list3 = MergeSort.mergeSort(list3);
         System.out.println("list sorted: " + list3);
